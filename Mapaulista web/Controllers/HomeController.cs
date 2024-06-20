01807d19b6ee417b584aeec0b8aed2ae3e8dd1b7
Kinho337 <kinho.olp@gmail.com>
2024-06-19 22:57:23 -0300

    criando duas telas

--- Mapaulista web/Controllers/HomeController.cs
+++ Mapaulista web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Mapaulista_web.Controllers
             return View();
         }
 
+        public IActionResult TelaInicial()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -28,5 +33,7 @@ namespace Mapaulista_web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
